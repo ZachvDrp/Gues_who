d7b58002772b300a4c93163b6813b122178dabec
ZachvDrp <z.vdorp@gmail.com>
2022-05-31 22:03:25 +0200

    fixed the bug of not awnsering the Choose_Character variable

--- Test_Guess_Who.py
+++ Test_Guess_Who.py
@@ -1,7 +1,7 @@
 #Choose a random character
 import random
 
-Choose_Character = random.choice(["Alex", "Alfred", "Anita", "Anne"])
+Choose_Character = random.choice(["Alex", "Anita"])
 
 #Set variables for the characteristics
 
@@ -21,14 +21,6 @@ if Choose_Character == "Alex":
     Facial_Hair = "Mustache"
     Jewellery = "No"
     Wears_Hat = "No"
-elif Choose_Character == "Alfred":
-    Gender = "Men"
-    Hair_Color = "Red"
-    Hair_Length = "Long"
-    Nose_Size = "Small"
-    Facial_Hair = "Mustache"
-    Jewellery = "No"
-    Wears_Hat = "No"
 elif Choose_Character == "Anita":
     Gender = "Women"
     Hair_Color = "Blond"
@@ -37,28 +29,30 @@ elif Choose_Character == "Anita":
     Facial_Hair = "No"
     Jewellery = "No"
     Wears_Hat = "No"
-elif Choose_Character == "Anne":
-    Gender = "Women"
-    Hair_Color = "Black"
-    Hair_Length = "Short"
-    Nose_Size = "Big"
-    Facial_Hair = "No"
-    Jewellery = "Earrings"
-    Wears_Hat = "No"
 
 #Loop questions
 Character_Guess = ""
 
 while Character_Guess != Choose_Character:
     #Questions
-    Question = input("What question would you like to ask? ").lower()
+    Question = input("What question would you like to ask? \n").lower()
     if Question == "is it a men?" or Question == "is it a men":
         if Gender == "Men":
             print("Yes")
         else:
             print("No")
+    elif Question == "is it a women?" or Question == "is it a women":
+        if Gender == "Women":
+            print("Yes")
+        else:
+            print("No")
     elif Question == "is the hair color blond?" or Question == "is the hair color blond":
         if Hair_Color == "Blond":
+            print("Yes")
+        else:
+            print("No")
+    elif Question == "is the hair color black?" or Question == "is the hair color black":
+        if Hair_Color == "Black":
             print("Yes")
         else:
             print("No")
@@ -68,20 +62,8 @@ while Character_Guess != Choose_Character:
             Character_Guess = Choose_Character
         else:
             print("No, try again")
-    elif Question == "is it alfred?" or Question == "is it alfred":
-        if Choose_Character == "Alfred":
-            print("Yes, you won the game. \nCongratulations")
-            Character_Guess = Choose_Character
-        else:
-            print("No, try again")
     elif Question == "is it anita?" or Question == "is it anita":
         if Choose_Character == "Anita":
-            print("Yes, you won the game. \nCongratulations")
-            Character_Guess = Choose_Character
-        else:
-            print("No, try again")
-    elif Question == "is it anne?" or Question == "is it anne":
-        if Choose_Character == "Anne":
             print("Yes, you won the game. \nCongratulations")
             Character_Guess = Choose_Character
         else:
